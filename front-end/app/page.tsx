2583a247d82cdcd73db67f89e66cfc4b1324026d
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-10 08:41:17 +0800

    deploy bug fixed

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,7 +1,6 @@
-import { currentUser } from "@clerk/nextjs/server";
 export default async function Home() {
   const res = await fetch(`${process.env.BACKEND_URL}/user`);
   const data = await res.json();
-  // console.log("data :", data);
+  console.log("data :", data);
   return <div>Hello Iam Home </div>;
 }
