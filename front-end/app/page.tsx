30b3826d00e9b1f903b508b3c9cb6c2d3402c4c5
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-12-10 08:44:38 +0800

    fetch is gotten rid of

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,6 +1,3 @@
 export default async function Home() {
-  const res = await fetch(`${process.env.BACKEND_URL}/user`);
-  const data = await res.json();
-  console.log("data :", data);
   return <div>Hello Iam Home </div>;
 }
